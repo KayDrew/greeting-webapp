6f5cd841460dce333f5817204219eeea42828df6
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-08-16 23:18:57 +0200

    Update sql.js

--- sql.js
+++ sql.js
@@ -1,9 +1,7 @@
 import pkg from 'pg';
 
 
-
 export  default    function setUsers(){
-	
 	
 const { Pool } = pkg;
 
@@ -14,33 +12,55 @@ const itemsPool = new Pool({
 
 let count=0;
 let names={};
+let individual={};
 
 
 async function setUser(name,language){
 
 let username=name;
 let names1={};
+let count=1;
 
 try {
-	const result=  await itemsPool.query(  `SELECT * FROM  users.users`  );         
-        
+	const result=  await itemsPool.query(  `SELECT * FROM  users.name`  );              
         	
-     names1= result.rows;
+     names1= result.rows[0];
      
+     
+     if(names1.length<1){
+
+if(username && language){
+		
+        const newItem = await itemsPool.query(
+            `INSERT INTO users.name (name,count) VALUES ($1,$2)`, [name,count]
+             );
+        }
+}     
+     else {     
+     	
+     	if(username && language){
+     	
      for(let i=0;i<names1.length;++i){
 
 var user=names1[i];
-     if(username===user.name){
 
+     if(username===user.name){
+     	
+     count=user.count+1;
+ const update=await itemsPool.query(`UPDATE users.name SET  count=$2 WHERE name=$1`,[username, count]);
 username="";
 }
+}
+
 }
 
 	if(username && language){
 		
         const newItem = await itemsPool.query(
-            `INSERT INTO users.users (name) VALUES ($1)`, [name]
+            `INSERT INTO users.name (name,count) VALUES ($1,$2)`, [name,count]
              );
+        }
+        
         }
         
     } catch (error) {
@@ -56,67 +76,52 @@ username="";
 try {
 	
         const items = await itemsPool.query(
-            `SELECT * FROM  users.users`           
+            `SELECT * FROM  users.name`           
         );
         
         let allItems= items;
         
-      
-        count=allItems.rows.length;
-        
-        
-       // console.log(count);
-        
- 
+       count=allItems.rows.length;
+             
     } catch (error) {
         console.log(error);
         res.status(500).send(error.message)
-    } 
-   
-  // return count;
+    }    
 
 }   
 
 
 async function  setNames(){
 	
-	
 try {
 const result=      await itemsPool.query(
-         `SELECT * FROM  users.users`           
+         `SELECT name FROM  users.name`           
         );
         	
      names= result.rows;
-     //console.log(names);
-      
-
- 
-//console.log(names);
+     
     } catch (error) {
         console.log(error);
         res.status(500).send(error.message)
     } 
-
-
-//return names;
-
 
 }
 
 
 async function deleteData(){
+	
 try {
-         await itemsPool.query(
-            `DELETE  FROM  users.users`           
+         
+           await itemsPool.query(
+            `DELETE  FROM  users.name`           
         );
-//console.log(names);
+        
     } catch (error) {
         console.log(error);
         res.status(500).send(error.message)
     } 
 
 }
-
 
 async function  getNames(){
 
@@ -128,6 +133,30 @@ async function getCount(){
 return count;
 }
 
+
+async  function  setIndividual(name){
+	
+		
+try {
+	const result=  await itemsPool.query(  `SELECT * FROM  users.name WHERE name=$1` ,[name]);             	
+     individual= result.rows[0];
+     
+console.log(individual);
+
+
+     }catch(err){
+     	
+console.log(err);
+}
+
+
+}
+
+async function getIndividual(){
+
+return individual;
+}
+
     return{
 
 setUser,
@@ -135,7 +164,10 @@ setCount,
 setNames,
 getCount, 
 getNames,
-deleteData 
+deleteData,
+setIndividual,
+getIndividual 
+
 
 }
     }
