61f67c922957149bd2a120a1064d6b42478a0c87
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-08-21 18:16:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,118 +1,45 @@
 import 'dotenv/config';
-//console.log(process.env.DATABASE_URL);
 import express from 'express';
 import bodyParser from 'body-parser';
 import { engine } from 'express-handlebars';
-import greet from './greet.js';
 import flash from 'express-flash';
 import session from 'express-session';
 import pkg from 'pg-promise';
-
 import setUsers from './sql.js';
 
 const connectionString = process.env.URL;
-
-
 const Pool = pkg();
-
 const db = Pool({
     connectionString,
     ssl: true
 });
-const app = express();
 
+const app = express();
 
 app.use(express.static('public'));
 app.engine('handlebars', engine());
-
 app.set('view engine', 'handlebars');
-
 app.set('views', './views');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
+app.use(flash());
 app.use(express.json());
-
 app.use(session({
   secret: "no secret",
   resave: false,
   saveInitialized: false
 }));
 
-let setGreeted= new setUsers(db);
 
-app.use(flash());
+let setGreeted= setUsers(db);
 
-const user = greet();
-
-app.get('/',async function (req, res,next) {
-
-  req.flash("error", user.getError());
-  let result= await setGreeted.setCount();
-  let count= await setGreeted.getCount()
- 
-    
-  res.render('index', {
-    greeting: user.getGreeting(),
-    title: "Home",
-    count: count
-
-  });
-
-}
-);
-
-app.post('/names',async function (req, res,next) {
-
-  let username=req.body.username;
-  let language = req.body.language;
-
-user.setName(username);
-
-  user.setGreeting(language);
- 
-  user.getError();
-  
-  let name=user.getName();
-  let greeted= await setGreeted.setUser(name,language);
-
-  res.redirect('/');
-
-});
-
-
-app.get("/greeted", async function (req, res,next){
-	
-await setGreeted.setNames();
-
-res.render ('greeted',{
- usersGreeted:await setGreeted.getNames()
-
-});
-
-});
-
-
-app.get("/counter/:name",async function (req, res,next){
-
-let username=req.params.name;
-let setGreetedUser= await setGreeted.setIndividual(username);
-let greetedUser= await setGreeted.getIndividual();
-
-res.render("counter",{
-	name: greetedUser
-
-});
-
-});
-
-app.post("/deleteData", async function(req,res,next){
-
-let result= await setGreeted.deleteData();
-res.redirect('/');
-
-});
+app.get('/',setGreeted.getCount);
+app.post('/names',setGreeted.setUser);
+app.get("/greeted", setGreeted.getNames);
+app.get("/counter/:name",setGreeted.getIndividual);
+app.post("/deleteData", setGreeted.deleteData);
 
 let PORT = process.env.PORT || 5432;
 
