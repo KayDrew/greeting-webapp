97699ae733f3a5b65ab58d6badbb1625dd176109
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-08-22 22:32:36 +0200

    Update sql.test.js

--- test/sql.test.js
+++ test/sql.test.js
@@ -1,8 +1,7 @@
+import 'dotenv/config';
 import assert from 'assert';
 import userData from '../sql.js';
 import pkg from 'pg-promise';
-
-// we are using a special test database for the tests
 const connectionString = process.env.URL;
 const Pool = pkg();
 const db = Pool({
@@ -14,30 +13,133 @@ let user= userData(db);
 
 describe('The greeting web app',async function(){
 	
- 
-     
 beforeEach(async function () {
-        try {
+	
+    try {
+        	
        await user.deleteData();
            }catch(err){
 
-console.log(err);
+         console.log(err);
 }
         
 }  );
       
-          
+   
+
+ it('should return the total number of the users that have been greeted', async function(){
+       assert.equal(0, await user.getCount());
+});
+        
+    
+    
+    
+    it('should return the name of the user/s greeted', async function(){
+                          
+   let arr=[];
+                            
+  try {
+                 	
+       await user.setUser("Reggie","Swahili");
+       arr=await user.getNames();
        
-    it('should pass the db test', async function(){
-             
-        assert.equal(0,user.getNames());
+           }catch(err){
+
+        console.log(err);
+}
+
+        assert.equal("Reggie",arr[0].name);
 
     });
+    
+    
+    
+     it('should return the total times a specific user has been greeted', async function(){
+     	
+     let individual={};
 
+     try {
+            	
+      await user.setUser("Reggie","Swahili");
+      await user.setUser("Reggie","Setswana");
+      individual =await user.getIndividual("Reggie");
+       
+           }catch(err){
+
+       console.log(err);
+}
+     	
+       assert.equal(2,individual.count);
+});
+    
+    
+     
+  it('should not add name to database if user has already been greeted', async function(){
+     	
+     let arr=[];
+     
+    try {
+       await user.setUser("Drew","Swahili");
+       await user.setUser("Drew","Setswana");
+       arr=await user.getNames();
+  
+        }catch(err){
+
+     console.log(err);
+   }
    
+       assert.equal(1,arr.length);
+
+});
+
+
+    
+ it('should delete all the user data', async function(){
+     	
+   let arr=[];
    
+    try {
+       await user.setUser("Leo","Swahili");
+       await user.setUser("Leo","Setswana");     
+       arr=await user.deleteData();
+       arr=await user.getNames();
+       
+           }catch(err){
+
+console.log(err);
+}
+assert.equal(0,arr.length);
+
+});
+
+
+     it('should be able to add  more users', async function(){
+     	
+   let arr=[];
+   
+  try {
+    	
+       await user.setUser("Leo","Swahili");
+       await user.setUser("Sara","Setswana");
+       await user.setUser("Sipho","Setswana");
+       arr=await user.getNames();
+       
+       
+           }catch(err){
+
+       console.log(err);
+}
+
+assert.equal(3,arr.length);
+
+});
+     	
+
        after(function () {
         db.$pool.end;
     });
+    
+    
+    
    
 });
