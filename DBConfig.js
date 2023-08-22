3d47c9ce9fd83d6d0b92ee607d08dd7e3e77b676
Kabelo Drew Tshehla <66612725+KayDrew@users.noreply.github.com>
2023-08-22 22:38:24 +0200

    Delete DBConfig.js

--- DBConfig.js
+++
@@ -1,9 +0,0 @@
-import { Client } from ('pg');
-
-const client= new Client({
-    connectionString: process.env.URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
-module.exports = client;
